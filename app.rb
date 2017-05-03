8ba2569fbfd0af402186a00b2d16e5cf4233fa66
Sowmya Dinavahi and Trevor Fast <trevor@>
2017-05-03 09:28:49 -0700

    Add sinatra and check the result page

--- app.rb
+++ app.rb
@@ -1,87 +1,43 @@
 require("sinatra")
-
 require("sinatra/reloader")
-
 require("./lib/task")
-
 require("./lib/list")
-
 also_reload("lib/**/*.rb")
-
 require 'pg'
-
-
 
 DB = PG.connect({:dbname => 'to_do'})
 
-# DB = PG.connect({:dbname => 'to_do_test'})
-
-
-
 get("/") do
-
   @lists = List.all()
-
   erb(:index)
-
 end
-
-
 
 get '/lists/new' do
-
   erb :list_form
-
 end
-
-
 
 get '/lists' do
-
   @lists = List.all
-
   erb :lists
-
 end
-
-
 
 post '/lists' do
-
   name = params.fetch('name')
-
   list = List.new({:name => name, :id => nil})
-
   list.save
-
   erb :list_success
-
 end
-
-
 
 get '/lists/:id' do
-
   @list = List.find(params.fetch('id').to_i)
-
   erb :list
-
 end
 
-
-
 post '/tasks' do
-
   description = params.fetch('description')
-
   list_id = params.fetch('list_id').to_i
-
   @list = List.find(list_id)
-
   @task = Task.new({:description => description, :list_id => list_id})
-
   @task.save
-
   erb :task_success
-
 end
