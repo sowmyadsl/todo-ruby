09f1fc4b958ad1e6afd07f98e3b1d862baf061e9
sowmyadsl <sowmya.dsl@gmail.com>
2017-05-08 10:41:05 -0700

    Refactor it using active record

--- app.rb
+++ app.rb
@@ -1,43 +1,32 @@
 require("sinatra")
 require("sinatra/reloader")
+require('sinatra/activerecord')
 require("./lib/task")
-require("./lib/list")
 also_reload("lib/**/*.rb")
 require 'pg'
 
-DB = PG.connect({:dbname => 'to_do'})
 
 get("/") do
-  @lists = List.all()
+  @tasks = Task.all()
   erb(:index)
-end
-
-get '/lists/new' do
-  erb :list_form
-end
-
-get '/lists' do
-  @lists = List.all
-  erb :lists
-end
-
-post '/lists' do
-  name = params.fetch('name')
-  list = List.new({:name => name, :id => nil})
-  list.save
-  erb :list_success
-end
-
-get '/lists/:id' do
-  @list = List.find(params.fetch('id').to_i)
-  erb :list
 end
 
 post '/tasks' do
   description = params.fetch('description')
-  list_id = params.fetch('list_id').to_i
-  @list = List.find(list_id)
-  @task = Task.new({:description => description, :list_id => list_id})
-  @task.save
+  task = Task.new({:description => description, :done => false})
+  task.save
   erb :task_success
+end
+
+get('/tasks/:id/edit') do
+  @task = Task.find(params.fetch("id").to_i())
+  erb(:task_edit)
+end
+
+patch('/tasks/:id') do
+  description = params.fetch('description')
+  @task = Task.find(params.fetch("id").to_i())
+  @task.update({:description => description})
+  @tasks = Task.all
+  erb(:index)
 end
--- lib/task.rb
+++ lib/task.rb
@@ -1,35 +1,8 @@
-class Task
-  # @@all_tasks = []
+class Task < ActiveRecord::Base
 
-  attr_reader(:description, :list_id)
+  belongs_to(:list)
 
-  define_method(:initialize) do |attributes|
-    @description = attributes.fetch(:description)
-    @list_id = attributes.fetch(:list_id)
-  end
-
-  define_singleton_method(:all) do
-    # @@all_tasks
-    returned_tasks = DB.exec("SELECT * FROM tasks;")
-    tasks = []
-    returned_tasks.each() do |task|
-      description = task.fetch('description')
-      list_id = task.fetch('list_id').to_i
-      tasks.push(Task.new({:description => description, :list_id => list_id}))
-    end
-    tasks
-  end
-
-  define_method(:save) do
-    #  @@all_tasks.push(self)
-    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
-  end
-
-  # define_singleton_method(:clear) do
-  #   @@all_tasks = []
-  # end
-
-  define_method(:==) do |another_task|
-    self.description.==(another_task.description).&(self.list_id.==(another_task.list_id))
-  end
+  scope(:not_done, -> do
+    where({:done => false})
+  end)
 end
